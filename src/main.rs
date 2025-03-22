021d9c4b7b312d5c2c4e6ab740ecdd1989222e6f
rhysm <rhys.link.0919@gmail.com>
2025-03-22 01:59:43 -0500

    Got models imported and animating, a lot to learn

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,97 @@
-use raylib::prelude::*;
+use std::f32::consts::PI;
 
+use bevy::{
+    pbr::CascadeShadowConfigBuilder, prelude::*, scene::SceneInstanceReady
+};
 
+const MODEL_PATH: &str = "robot.glb";
 
 fn main() {
-    let (mut rl, thread) = open_window("Rings Field");
-    //let (w, h) = (1920, 1080); //working with some example code some translations are needed
-
-    let mut camera = Camera3D::perspective(
-        Vector3::new(4.0, 4.0, 4.0), // Position
-        Vector3::new(0.0, 1.0, -1.0), // Target
-        Vector3::new(0.0, 1.0, 0.0), // Up vector
-        90.0,); // FOV
-
-
-    let mut model = rl.load_model(&thread ,"assets/robot.glb").unwrap();
-    let position = Vector3::new(0.0, 0.0, 0.0);
-
-    let mut animation_index: u32= 2;
-    let mut animation_current_frame: u32 = 0;
-    let animation_model = rl.load_model_animations(&thread, "assets/robot.glb").expect("Failed to load animation");
-    let animation_count: i32 = animation_model.len() as i32;
-    rl.set_target_fps(60);
-
-    while !rl.window_should_close() {
-        rl.update_camera(&mut camera, CameraMode::CAMERA_FREE);
-
-        if rl.is_mouse_button_pressed(MouseButton::MOUSE_BUTTON_LEFT) {
-            animation_index = (animation_index + 1) % animation_count as u32;
-
-        } else if rl.is_mouse_button_pressed(MouseButton::MOUSE_BUTTON_RIGHT) {
-            animation_index = (animation_index + animation_count as u32 - 1) % animation_count as u32;
-        }
-
-
-
-        animation_index = animation_index % animation_count as u32;
-
-        let anim = &animation_model[animation_index as usize];
-        if anim.frameCount > 0 {
-            animation_current_frame = (animation_current_frame + 1) % anim.frameCount as u32;
-
-            //dbg!(animation_index, animation_count, anim.frameCount, anim.boneCount, model.boneCount);
-            rl.update_model_animation(&thread, &mut model, anim, animation_current_frame as i32);
-        } else {
-            eprintln!("Error: Animation {} has 0 frames!", animation_index);
-        }
-        let mut drawing = rl.begin_drawing(&thread);
-        drawing.clear_background(Color::BLACK);
-
-        drawing.draw_mode3D(camera, |mut mode_3d, _camera| {
-            mode_3d.draw_model(&model, position, 0.2, Color::WHITE);
-            mode_3d.draw_grid(10, 1.0);
-        });
-
-        drawing.draw_fps(10, 10)
-
-    }
-
-
+    App::new()
+        .insert_resource(AmbientLight {
+            color: Color::WHITE,
+            brightness: 2000.0,
+            ..default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup_mesh)
+        .add_systems(Startup, setup_camera_and_env)
+        .run();
 }
 
-fn open_window(name: &str) -> (raylib::RaylibHandle, raylib::RaylibThread) {
-    let (mut rl, thread) = raylib::init()
-        .size(1920, 1080)
-        .title(name)
-        .build();
+#[derive(Component)]
+struct AnimationToPlay {
+    graph_handle: Handle<AnimationGraph>,
+    index: AnimationNodeIndex,
+}
 
-    rl.set_target_fps(120);
+fn setup_mesh(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut graphs: ResMut<Assets<AnimationGraph>>,
+) {
+    let (graph, index) = AnimationGraph::from_clip(
+        asset_server.load(GltfAssetLabel::Animation(1).from_asset(MODEL_PATH)),
+    );
 
-    (rl, thread)
+    let graph_handle = graphs.add(graph);
+
+    let animation_to_play = AnimationToPlay {
+        graph_handle,
+        index,
+    };
+
+    let mesh_scene = SceneRoot(asset_server.load(GltfAssetLabel::Scene(0).from_asset(MODEL_PATH)));
+
+    commands
+        .spawn((animation_to_play, mesh_scene))
+        .observe(play_animation_when_ready);
+}
+
+fn play_animation_when_ready(
+    trigger: Trigger<SceneInstanceReady>,
+    mut commands: Commands,
+    children: Query<&Children>,
+    animations_to_play: Query<&AnimationToPlay>,
+    mut players: Query<&mut AnimationPlayer>,
+) {
+    if let Ok(animation_to_play) = animations_to_play.get(trigger.entity()) {
+        for child in children.iter_descendants(trigger.entity()) {
+            if let Ok(mut player) = players.get_mut(child) {
+                player.play(animation_to_play.index).repeat();
+
+                commands.entity(child).insert(AnimationGraphHandle(animation_to_play.graph_handle.clone()));
+            }
+        }
+    }
+}
+
+fn setup_camera_and_env (
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>
+) {
+    commands.spawn((
+       Camera3d::default(),
+       Transform::from_xyz(25.0, 25.0, 25.0).looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
+    ));
+
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(500000.0, 500000.0))),
+        MeshMaterial3d(materials.add(Color::srgb(0.0, 0.0, 0.0))),
+    ));
+
+    commands.spawn((
+        Transform::from_rotation(Quat::from_euler(EulerRot::ZYX, 0.0, 1.0, -PI / 4.)),
+        DirectionalLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        CascadeShadowConfigBuilder {
+            first_cascade_far_bound: 200.0,
+            maximum_distance: 400.0,
+            ..default()
+        }
+        .build(),
+    ));
 }
